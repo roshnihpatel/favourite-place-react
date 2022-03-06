64868c95d75a15b69061774a7f65e6c519110d13
Roshni Patel <roshnihpatel@outlook.com>
2022-03-06 17:19:40 +0000

    added red valley as a fav place

--- src/components/FavouritePlace.tsx
+++ src/components/FavouritePlace.tsx
@@ -1,5 +1,23 @@
-function FavouritePlace(): JSX.Element {
-  return <></>;
+interface FavouritePlaceProps {
+  placeName: string;
+  location: { country: string; city: string };
+  description: string;
+  imageURL: string;
+  imageDescription: string;
+}
+
+function FavouritePlace(props: FavouritePlaceProps): JSX.Element {
+  return (
+    <section>
+      <h2>{props.placeName}</h2>
+
+      <img ref={props.imageURL} alt={props.imageDescription} />
+      <p>
+        {props.location.country},{props.location.city}
+      </p>
+      <p>{props.description}</p>
+    </section>
+  );
 }
 
 export default FavouritePlace;
